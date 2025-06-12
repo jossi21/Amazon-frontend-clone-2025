a39dc55f86346d3a8fb2f67b666f648fbc6434b5
jossi21 <ahushabush4@gmail.com>
2025-06-13 01:17:44 +0300

    final deploy commikt

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export const axiosInstant = axios.create({
   // local server of function firebase
-  baseURL: "http://127.0.0.1:5001/clone-81fd8/us-central1/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // baseURL: "http://127.0.0.1:5001/clone-81fd8/us-central1/api",
+  // headers: {
+  //   "Content-Type": "application/json",
+  // },
   // deployed version of amazon server on render.com
   baseURL: "https://amazon-backend-deploy-q17r.onrender.com/",
 });
